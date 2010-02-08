f55a1e441a2af2d0441c237cdb85c47fa9c75ee5
opsb <oliver@opsb.co.uk>
2010-02-08 12:05:08 +0000

    Using built in widget options. Broke out html rendering to separate method.

--- agenda.js
+++ agenda.js
@@ -4,31 +4,49 @@ $.widget("ui.agenda", {
 		this._render();
 	},
 
-	message : function() {
-		return this.options.message;
-	},
-
 	_render : function() {
-		
+		this._renderHtml();
+		this._addPositionInformationToTableCells();
+	},
+	
+	_renderHtml : function(options) {
 		this.element.html(
-			new EJS({url: 'template.ejs'}).render({
-				days: 			3,
-				columns: 		4,
-				startHour: 	9,
-				endHour: 		17
-			})
+			new EJS({url: 'template.ejs'}).render(this.options)
 		);
+	},
+	
+	_addPositionInformationToTableCells : function() {
+		var rows = this.element.find('table tbody tr')
 		
+		var rowIndex = 0;
+		var columnIndex = 0;
+		for( var day = 1; day <= this.options['days']; day++ ) {
+			for( var hour = this.options['startHour']; hour <= this.options['endHour']; hour++ ) {
+				var row	= rows.get(rowIndex);
+				for( var column = 1; column <= this.options['columns']; column++ ) {
+					var cell = $(row.cells[columnIndex]);
+					cell.data("day", 		day);
+					cell.data("column", column);
+					cell.data("hour", 	hour);
+					columnIndex++;
+				}
+				columnIndex = 0;
+				rowIndex++;
+			}
+		}
 	}
 
 });
 
 $.extend($.ui.agenda, {
 
-  getter : "message",
+  getter : "data options",
 
   defaults: {
-    message: "hello!"
+		days: 			3,
+		columns: 		4,
+		startHour: 	9,
+		endHour: 		17
   }
 
 });

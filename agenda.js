4a2aaeaadaf73554ec8c3c65441791ed6c78cca2
opsb <oliver@opsb.co.uk>
2010-02-09 01:17:55 +0000

    Extended Element with Element add('name of element')

--- agenda.js
+++ agenda.js
@@ -1,3 +1,9 @@
+Element.prototype.add = function(elementName) {
+  var element = document.createElement(elementName);
+  this.appendChild(element);
+  return element;
+};
+
 $.widget("ui.agenda", {
 
 	_init : function() {
@@ -11,29 +17,19 @@ $.widget("ui.agenda", {
 	
 	_renderGrid : function() {	
 
-		var table = $(document.createElement("table"));
-		table.css("width","100%");
-		table.show();
-		this.element.append(table);
-		
-		var tbody = $(document.createElement("tbody"));
-		table.append(tbody);
-		
-		var tr = $(document.createElement("tr"));
-		tbody.append(tr);
+		var table = this.element.add("table");
+		table.css("width","100%");		
+		var tbody = table.add("tbody");
 		
 		for( var day = 1; day <= this.options.days; day++ ) {	
 			for( var hour = this.options.startHour; hour <= this.options.endHour; hour++ ) {
-				var row = $(document.createElement("tr"));
-				tbody.append(row);
+				var row = tbody.add("tr");
 				for( var column = 1; column <= this.options.columns; column++ ) {
-					var cell = $(document.createElement("td"));
-					row.append(cell);
+					var cell = $(row.add("td"));
 					cell.data("day", 		day);
 					cell.data("column", column);
 					cell.data("hour",		hour);
 					cell.css("height",	this.options.periodHeight);
-					row.append(cell);
 				}
 			}
 		}
@@ -43,7 +39,6 @@ $.widget("ui.agenda", {
 	_renderEvents : function() {
 		self = this;
 		$(this.options.events()).each(function(index, event) {
-			console.log(event);
 			var eventElement = $(new EJS({url: 'event.ejs'}).render(event));
 			
 			eventElement.data("day", event.day			  );
